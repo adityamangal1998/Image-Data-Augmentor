6732768c0b6214e495326abec1c02edd70585c33
adityamangal1998 <adityamangal98@gmail.com>
2025-05-26 02:45:14 +0530

    fix: update setup to build the package correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='ImageDataAugmentor',
-    version='1.0.2',  # Incremented version
+    version='1.0.3',  # Incremented version
     description='Augment Image Data',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -12,14 +14,18 @@ setup(
     author='Aditya Mangal',
     author_email='adityamangal98@gmail.com',
     license="MIT",
-    packages=['ImageDataAugmentor'],
-    install_requires=['opencv-python>=4.5',
-                      'numpy>=1.17',
-                      'future>=0.17',
-                      'glob2>=0.7',
-                      'ConcurrentImageRead>=0.0.10'
-                      ],
-
+    packages=find_packages(),  # Automatically find all packages
+    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
+    package_data={
+        'ImageDataAugmentor': ['configs/*', 'augmentors/*', 'config.yaml'],  # Include specific files
+    },
+    install_requires=[
+        'opencv-python>=4.5',
+        'numpy>=1.17',
+        'future>=0.17',
+        'glob2>=0.7',
+        'ConcurrentImageRead>=0.0.10'
+    ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
